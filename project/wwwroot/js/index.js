632dbff4477f1eabdcf577dddb4cfad797a0aab2
Алексей Дириволков <dirivolkova@gmail.com>
2026-05-04 17:48:15 +0200

    first commit

---
+++ project/wwwroot/js/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const myButtonEl = document.getElementById('myButton');
+    if(myButtonEl) myButton.addEventListener('click', ()=>{
+        alert('Click button')
+    })
+})
